5da711bb58e238fda85ba4521bdaa8870f16ed2b
O <familye@naver.com>
2020-04-28 20:43:07 +0900

    Seperate Get, Post Button

--- app/src/main/java/com/example/freshapp/MainActivity.java
+++ app/src/main/java/com/example/freshapp/MainActivity.java
@@ -1,140 +1,28 @@
 package com.example.freshapp;
-import android.app.ProgressDialog;
-import android.os.Handler;
-import android.os.Message;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.lang.ref.WeakReference;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final int LOAD_SUCCESS = 101;
-
-    private String SEARCH_URL = "http://175.208.85.188:8421/AG1/1";
-    private String REQUEST_URL = SEARCH_URL;
-
-    private ProgressDialog progressDialog;
-    private TextView textviewJSONText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button buttonRequestJSON = (Button)findViewById(R.id.button_main_requestjson);
-        textviewJSONText = (TextView)findViewById(R.id.textview_main_jsontext);
-        textviewJSONText.setMovementMethod(new ScrollingMovementMethod());
-
-        buttonRequestJSON.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                progressDialog = new ProgressDialog( MainActivity.this );
-                progressDialog.setMessage("Please wait.....");
-                progressDialog.show();
-
-                getJSON();
-            }
-        });
     }
 
-
-
-    private final MyHandler mHandler = new MyHandler(this);
-
-
-    private static class MyHandler extends Handler {
-        private final WeakReference<MainActivity> weakReference;
-
-        public MyHandler(MainActivity mainactivity) {
-            weakReference = new WeakReference<MainActivity>(mainactivity);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-
-            MainActivity mainactivity = weakReference.get();
-
-            if (mainactivity != null) {
-                switch (msg.what) {
-
-                    case LOAD_SUCCESS:
-                        mainactivity.progressDialog.dismiss();
-
-                        String jsonString = (String)msg.obj;
-
-                        mainactivity.textviewJSONText.setText(jsonString);
-                        break;
-                }
-            }
-        }
+    public void onGetButtonClick(View v) {
+        Intent GetIntent = new Intent(getApplicationContext(), Getdata.class);
+        startActivity(GetIntent);
     }
 
-
-
-
-    public void  getJSON() {
-
-        Thread thread = new Thread(new Runnable() {
-
-            public void run() {
-
-                String result;
-
-                try {
-
-                    Log.d("hh", REQUEST_URL);
-                    URL url = new URL(REQUEST_URL);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-
-                    httpURLConnection.setReadTimeout(5000);
-                    httpURLConnection.setConnectTimeout(5000);
-                    httpURLConnection.setRequestMethod("GET");
-                    httpURLConnection.setDoOutput(false);
-
-                    StringBuilder sb = new StringBuilder();
-                    if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                        BufferedReader br = new BufferedReader(
-                                new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"));
-                        String line;
-                        while ((line = br.readLine()) != null) {
-                            sb.append(line).append("\n");
-                        }
-                        br.close();
-                        System.out.println("" + sb.toString());
-                    } else {
-                        System.out.println(httpURLConnection.getResponseMessage());
-                    }
-
-                    httpURLConnection.disconnect();
-
-                    result = sb.toString().trim();
-
-
-                } catch (Exception e) {
-                    result = e.toString();
-                }
-
-
-                Message message = mHandler.obtainMessage(LOAD_SUCCESS, result);
-                mHandler.sendMessage(message);
-            }
-
-        });
-        thread.start();
+    public void onPussButtonClick(View v) {
+        Intent PostIntent = new Intent(getApplicationContext(), Postdata.class);
+        startActivity(PostIntent);
     }
-
 }
